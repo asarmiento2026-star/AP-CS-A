56bfccbf6d1178d114a0df4b70687267f190ce88
Aaron <asarmiento2026@cchsdons.com>
2026-01-09 09:19:19 -0800

    Add files via upload

---
+++ ClassRelationshipsInterfacesAssignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.classrelationshipsinterfacesassignment;
+
+/**
+ *
+ * @author asarmiento2026
+ */
+public class ClassRelationshipsInterfacesAssignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
